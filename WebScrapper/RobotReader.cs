5ea2f3dee30e40a155ffdca9c2db35eb046cb0db
Kevin Borup - Desktop <kevin.borup@outlook.com>
2024-06-11 08:21:43 +0200

    Robot parsing V1

--- WebScrapper/RobotReader.cs
+++ WebScrapper/RobotReader.cs
@@ -11,24 +11,23 @@ namespace WebScrapper
 {
     internal class RobotReader
     {
-        public bool allowed = true;
+        public bool Allowed = true;
 
         public bool AllowedMode = false;
-        public List<string> allowedPages = new List<string>();
-        public List<string> disallowedPages = new List<string>();
+        public List<string> AllowedPages = new List<string>();
+        public List<string> DisallowedPages = new List<string>();
 
         public int CrawlDelay = 0;
 
         public bool VisitTimeSet = false;
         public DateTime StartVisitTime = DateTime.MinValue;
-        public DateTime EndVisitTime = DateTime.MinValue;
+        public DateTime EndVisitTime = DateTime.MaxValue;
 
         public bool RequestRateSet = false;
         public int RequestRate = 0;
         public int RequestInterval = 0;
 
-        public bool SitemapIncluded = false;
-        public List<string> sitemap = new List<string>();
+        public List<string> Sitemaps = new List<string>();
 
         public RobotReader(string url)
         {
@@ -37,26 +36,82 @@ namespace WebScrapper
 
         private async void ReadRobotTxt(string url)
         {
-            var disallowedPages = new ConcurrentBag<string>();
-
             url = Path.Combine(url, "robot.txt");
 
             string contentOfRobotTxt = await new HttpClient().GetStringAsync(url);
 
             bool relevantUserAgent = false;
 
+            int lastUserAgentLine = 0;
+            int index = 0;
             foreach (var line in contentOfRobotTxt.Split('\n'))
             {
-                if (line.Equals("User-agent: *")) relevantUserAgent = true;
+                if (line.Equals("User-agent: *"))
+                {
+                    relevantUserAgent = true;
+                    Allowed = true;
+                    lastUserAgentLine = index;
+                }
+                else if (line.StartsWith("User-agent:"))
+                {
+                    if (index - 1 ==  lastUserAgentLine) 
+                    {
+                        lastUserAgentLine = index;
+                        continue; 
+                    }
+
+                    if (!relevantUserAgent) Allowed = false;
+                    relevantUserAgent = false;
+                }
+
 
                 if (relevantUserAgent)
                 {
                     if (line.StartsWith("Disallow:"))
                     {
-                        disallowedPages.Add(line.Remove(0, "Disallow:".Length).Trim());
+                        DisallowedPages.Add(line.Remove(0, "Disallow:".Length).Trim());
+                        if (line.Equals("Disallow: /")) Allowed = false;
+                    }
+
+                    if (line.StartsWith("Allow:"))
+                    {
+                        AllowedMode = true;
+                        AllowedPages.Add(line.Remove(0, "Allow:".Length).Trim());
+                    }
+
+                    if (line.StartsWith("Sitemap:"))
+                    {
+                        Sitemaps.Add(line.Remove(0, "Sitemap:".Length).Trim());
+                    }
+
+                    if (line.StartsWith("Crawl-delay:"))
+                    {
+                        CrawlDelay = int.Parse(line.Remove(0, "Crawl-delay:".Length).Trim());
+                    }
+
+                    if (line.StartsWith("Visit-time:"))
+                    {
+                        VisitTimeSet = true;
+                        var times = line.Remove(0, "Visit-time:".Length).Trim().Split('-');
+                        StartVisitTime = DateTime.Parse(times[0]);
+                        EndVisitTime = DateTime.Parse(times[1]);
+                    }
+
+                    if (line.StartsWith("Request-rate:"))
+                    {
+                        RequestRateSet = true;
+                        var xy = line.Remove(0, "Request-rate:".Length).Trim().Split('/');
+                        RequestRate = int.Parse(xy[0]);
+                        RequestInterval = int.Parse(xy[1]);
                     }
                 }
 
+                index++;
+            }
+
+            if (AllowedPages.Count > 0 && !Allowed) 
+            {
+                Allowed = true;
             }
         }
     }
